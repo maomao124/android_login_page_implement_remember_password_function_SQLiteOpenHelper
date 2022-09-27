81a303325ca3c51d67cbcdfdc0ef3333711d9110
mao <1296193245@qq.com>
2022-09-27 10:42:10 +0800

    android_login_page_implement_remember_password_function_SQLiteOpenHelper

---
+++ app/src/main/java/mao/android_login_page_implement_remember_password_function_sqliteopenhelper/MainActivity.java
@@ -0,0 +1,16 @@
+package mao.android_login_page_implement_remember_password_function_sqliteopenhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/mao/android_login_page_implement_remember_password_function_sqliteopenhelper/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+package mao.android_login_page_implement_remember_password_function_sqliteopenhelper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest
+{
+    @Test
+    public void addition_isCorrect()
+    {
+        assertEquals(4, 2 + 2);
+    }
+}
